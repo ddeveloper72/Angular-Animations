7d503c98d5f178721a99dd1d11b919b99ea22b2f
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-27 12:31:41 +0000

    Added keyframes to control the annimation process in lsit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { state, style, trigger, transition, animate } from '@angular/animations';
+import { state, style, trigger, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -55,10 +55,10 @@ import { state, style, trigger, transition, animate } from '@angular/animations'
           1000,
           style({
             borderRadius: '50px'
-          }),
+          })
         ),
         animate(500)
-      ]),
+      ])
     ]),
     trigger('list1', [
       state(
@@ -76,10 +76,55 @@ import { state, style, trigger, transition, animate } from '@angular/animations'
         animate(300)
       ]),
       transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))
+        animate(
+          300,
+          style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })
+        )
+      ])
+    ]),
+    trigger('list2', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })
+        )
       ])
     ])
   ]
