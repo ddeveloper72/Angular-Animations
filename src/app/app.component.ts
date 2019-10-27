2319e975207e4aeb99444b1a5afb061e8a581831
ddeveloper72 <duncan.niamh@gmail.com>
2019-10-27 10:30:38 +0000

    Added transition state styling & corrected css naming type suntax.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
-import { state, style, trigger } from '@angular/animations';
+import { state, style, trigger, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css'],
+  styleUrls: ['./app.component.css'],
   animations: [
     trigger('divState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)'
-      })),
-      state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(100px)'
-      }))
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)'
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'blue',
+          transform: 'translateX(100px)'
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
     ])
   ]
 })
@@ -22,7 +30,17 @@ export class AppComponent {
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state === 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete() {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
